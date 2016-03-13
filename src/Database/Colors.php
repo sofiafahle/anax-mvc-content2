2e64900a305714b333117caa71975b0cabafe44e
sofiafahle <sofiafahlesson@live.se>
2016-03-13 15:15:48 +0100

    Added files via upload

---
+++ src/Database/Colors.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Anax\Database;
+ 
+/**
+ * Class with table for testing CDatabaseModel
+ *
+ */
+class Colors extends CDatabaseModel implements \Anax\DI\IInjectionAware
+{
+	use \Anax\DI\TInjectable;
+	
+	public function init()
+	{
+		$di = new Anax\DI\CDIFactoryDefault();
+		
+		$di->setShared('db', function() {
+			$db = new \Mos\Database\CDatabaseBasic();
+			$db->setOptions(['dsn' => "sqlite:memory::", "verbose" => false]);
+			$db->connect();
+			return $db;
+		});
+		
+		$this->db->dropTableIfExists("test");
+		$this->db->execute();
+		$this->db->createTable(
+			'test',
+			[
+				'id'    => ['integer', 'auto_increment', 'primary key', 'not null'],
+				'name'  => ['varchar(20)'],
+				'color' => ['varchar(20)'],
+			]
+		);
+		$this->db->execute();
+		$this->db->insert(
+			'test',
+			['name', 'color']
+		);
+		$this->db->execute(['Sofia', 'Red']);
+		$this->db->execute(['Olle', 'Blue']);
+		
+		
+        /*
+        $this->db->select("*")
+            ->from("test")
+        ;
+        var_dump($this->db->executeFetchAll());
+        */
+		
+	}
+	
+}
